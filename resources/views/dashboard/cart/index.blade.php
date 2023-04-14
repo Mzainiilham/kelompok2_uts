eb6dc711e62876ad04f04b102d4860f03a215acd
unknown <robitera11@gmail.com>
2023-04-14 16:01:56 +0700

    Halaman Cart

--- resources/views/dashboard/cart/index.blade.php
+++ resources/views/dashboard/cart/index.blade.php
@@ -8,14 +8,14 @@
         <div class="col-3 mt-2 mb-1">
             <div class="card" style="min-width: 18rem;">
                 <div class="card-body">
-                    <h5 class="card-title"> Nama Produk : {{ $cart->$product->name }} </h5>
-                    <p class="card-text my-1"> Kategori Produk : {{$cart->$product->name}}</p>
-                    <p class="card-text text-muted my-1">{{$cart->$product->price}}</p>
+                    <h5 class="card-title"> Nama Produk : {{ $cart->product->name }} </h5>
+                    <p class="card-text my-1"> Kategori Produk : {{$cart->product->name}}</p>
+                    <p class="card-text text-muted my-1">{{$cart->product->price}}</p>
                     <p class="card-text text-muted my-1"> Kuantitas Produk : {{$cart->qty}}</p>
                 </div>
                 <div class="card-body">
                     <h5 class="card-title">Deskripsi</h5>
-                    <p class="card-text">{{ $cart->$product->description }}</p>
+                    <p class="card-text">{{ $cart->product->description }}</p>
                 </div>
         </div>
     </div>
