2790f7ad80dd0bb2307758d65ece62f175d20a87
lukmanyahya <105480771+lukmanyahya@users.noreply.github.com>
2023-04-14 18:53:39 +0700

    Update create.blade.php

--- resources/views/dashboard/transaction/create.blade.php
+++ resources/views/dashboard/transaction/create.blade.php
@@ -12,12 +12,6 @@
         <label for="exampleInputEmail1" class="form-label">Total</label>
         <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="total">
       </div>
-        <div class="mt-3">
-        <label for="exampleInputEmail1" class="form-label">User ID</label>
-        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="user_id">
-      </div>
-     
-      
       
       <button type="submit" class="btn btn-primary mt-3">Tambah dong</button>
       
